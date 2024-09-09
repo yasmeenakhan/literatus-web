73c30b3229272cf7b9ef7be895e9600987d9e86d
yasmeenakhan <yasmeenak@gmail.com>
2024-09-09 13:04:43 -0700

    Changed profile_image column to text type

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    profile_image = db.Column(db.String(256))
+    profile_image = db.Column(db.Text)
     books = db.relationship('Book', backref='user', lazy=True)
 
     def set_password(self, password):
