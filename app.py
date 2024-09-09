4d37e741745eca06236149b8c40cab2aeeda9e33
yasmeenakhan <yasmeenak@gmail.com>
2024-09-09 10:01:37 -0700

    prepare to deploy

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 import os
 
-app = Flask(__name__)
 
-database_url = os.environ.get('DATABASE_URL')
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///literatus.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
